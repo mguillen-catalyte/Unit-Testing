9679dca0dc6c5262a780390c49812181a828a89d
Maria Guillen <mguillen@catalyte.io>
2025-09-17 15:21:39 -0500

    refactored Loops exercise and created test cases

---
+++ test_unit_testing_exercise.py
@@ -0,0 +1,22 @@
+from unit_testing_exercise import (
+    sum_of_numbers,
+    minimum_number,
+    maximum_number,
+    average_number,
+)
+
+
+def test_sum_of_numbers():
+    assert sum_of_numbers([2, 6, 9]) == 17
+
+
+def test_minimum_number():
+    assert minimum_number([2, 6, 9]) == 2
+
+
+def test_maximum_number():
+    assert maximum_number([2, 6, 9]) == 9
+
+
+def test_average_number():
+    assert average_number([1, 5, 9]) == 5
---
+++ unit_testing_exercise.py
@@ -0,0 +1,29 @@
+num_list = [1, 9, 10, 15, 22]
+
+
+def sum_of_numbers(num_list):
+    """
+    Finds the sum of all the numbers on the given list
+    """
+    return sum(num_list)
+
+
+def minimum_number(num_list):
+    """
+    Finds the minumum number on the list
+    """
+    return min(num_list)
+
+
+def maximum_number(num_list):
+    """
+    Finds the maximum number on the list
+    """
+    return max(num_list)
+
+
+def average_number(num_list):
+    """
+    Finds the average number from the list.
+    """
+    return sum(num_list) / len(num_list)
